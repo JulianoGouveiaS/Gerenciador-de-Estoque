0b730ae7edcbc2dd2c89ddeb7b9cbd3b1c8d1975
Juliano Gouveia <julianogouveia93@gmail.com>
2022-07-11 20:00:01 -0300

    commit 1

---
+++ src/test/java/com/geretq/gerenciadorEstoque/GerenciadorEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geretq.gerenciadorEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
